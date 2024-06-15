adf76c0b2ee8757a1a7bb1ddd02e075cc5f3af96
VyanPersad <vyanpersad@gmail.com>
2024-06-15 02:30:43 -0400

    Set up a new environment
    Set up the new environment and created some new functions.

---
+++ masks.py
@@ -0,0 +1,98 @@
+import numpy as np
+import cv2
+
+def shadowMsk(image):
+    #Shadow Mask
+    converted_c = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lshad = np.array([0, 0, 0], dtype="uint8")
+    ushad = np.array([180, 50, 15], dtype="uint8")
+    shadow_mask = cv2.inRange(converted_c, lshad, ushad)
+    #This should show the shadowed region with a green tint.
+    img_w_shadow_highlight = cv2.bitwise_and(converted_c, converted_c, mask=~shadow_mask)
+    return img_w_shadow_highlight
+
+def HSVskinMask(image):
+    lower = np.array([3, 15, 10], dtype="uint8")
+    upper = np.array([20, 255, 255], dtype="uint8")
+
+    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    skinMask = cv2.inRange(converted, lower, upper)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations=2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations=2)
+    # blur the mask to help remove noise
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    # apply the mask to the original image
+    skin = cv2.bitwise_and(image, image, mask=skinMask)
+    # Find contours in the skin mask
+    contours, _ = cv2.findContours(skinMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Initialize a list to store bounding boxes of non-black/skin regions
+    non_black_boxes = []
+    # Loop over the contours
+    for contour in contours:
+        # Get bounding box for each contour
+        x, y, w, h = cv2.boundingRect(contour)
+        if cv2.contourArea(contour) > 100:
+            non_black_boxes.append((x, y, w, h))
+        
+    # Create a copy of the original skin image
+    skin_cropped = skin.copy()
+    # Draw bounding boxes on the copy
+    # This only draws the reactangle bounding boxes
+    for box in non_black_boxes:
+        x, y, w, h = box
+        #img output,upper left, lower right, BGR Color, thickness
+        cv2.rectangle(skin_cropped, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Determine Largest Contour
+    # Get largest contour
+    largest_contour = max(contours, key=cv2.contourArea)
+    largest_contour_mask = np.zeros_like(skinMask)
+    cv2.drawContours(largest_contour_mask, [largest_contour], 0, 255, thickness=cv2.FILLED)
+
+    # Apply the mask to the original image
+    largest_contour_image = cv2.bitwise_and(image, image, mask=largest_contour_mask) 
+
+    return largest_contour_image
+
+def YCbCrskinMask(image):
+    lower = np.array([0, 138, 67], dtype="uint8")
+    upper = np.array([255, 173, 133], dtype="uint8")
+
+    converted = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+    skinMask = cv2.inRange(converted, lower, upper)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    skinMask = cv2.erode(skinMask, kernel, iterations=2)
+    skinMask = cv2.dilate(skinMask, kernel, iterations=2)
+    # blur the mask to help remove noise
+    skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+    # apply the mask to the original image
+    skin = cv2.bitwise_and(image, image, mask=skinMask)
+    # Find contours in the skin mask
+    contours, _ = cv2.findContours(skinMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Initialize a list to store bounding boxes of non-black/skin regions
+    non_black_boxes = []
+    # Loop over the contours
+    for contour in contours:
+        # Get bounding box for each contour
+        x, y, w, h = cv2.boundingRect(contour)
+        if cv2.contourArea(contour) > 100:
+            non_black_boxes.append((x, y, w, h))
+        
+    # Create a copy of the original skin image
+    skin_cropped = skin.copy()
+    # Draw bounding boxes on the copy
+    # This only draws the reactangle bounding boxes
+    for box in non_black_boxes:
+        x, y, w, h = box
+        #img output,upper left, lower right, BGR Color, thickness
+        cv2.rectangle(skin_cropped, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Determine Largest Contour
+    # Get largest contour
+    largest_contour = max(contours, key=cv2.contourArea)
+    largest_contour_mask = np.zeros_like(skinMask)
+    cv2.drawContours(largest_contour_mask, [largest_contour], 0, 255, thickness=cv2.FILLED)
+
+    # Apply the mask to the original image
+    largest_contour_image = cv2.bitwise_and(image, image, mask=largest_contour_mask) 
+
+    return largest_contour_image
---
+++ readFromFile.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+import cv2
+
+#This function is to be used within a loop the increment 
+#represents the individual file the loop will iterate through
+#where the file path is path to the file without the quotation marks
+#do not inlude the trailing forward slash
+filepath = None
+img_Array = []
+
+def readFromFile(filepath, increment):
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", help="path to the image file", default=f'{filepath}/{increment}')                
+    args = vars(ap.parse_args())
+    image = cv2.imread(args["image"])    
+    #Outputs a 300 x 300 px image
+    image = cv2.resize(image, (300, 300))
+    return image 
+
+def img_array(filepath):
+    for file in os.listdir(f'{filepath}/'):
+        img_Array.append(readFromFile(filepath, file))
+        #This will build an array of the images in the directory
+
+    return img_Array
